7c36af657cc2268d3a0cc4b47bc211013669939a
Julien-QTX <julien.quatravaux@live.fr>
2022-11-25 13:05:43 +0100

    Couleur

--- banque.js
+++ banque.js
@@ -1,79 +1,41 @@
-var compte = {
-    
-    // Crédite le compte d'un certain montant
-    crediter: function (montant) {
-        this.solde = this.solde + montant;
-    },
-    // Débite le compte d'un certain montant
-    debiter: function (montant) {
-        this.solde = this.solde - montant;
-    },
-    // Renvoie la description du compte
-    decrire: function () {
-        var description = "Titulaire : " + this.titulaire +
-            ", solde : " + this.solde + " euros";
-        return description;
-    }
-};
+/ récupérer les infos du form
+var compte = document.querySelector("#compte");
+var somme = document.querySelector("#somme");
 
-function W3docs()                                    
-{ 
-    var name = document.forms["RegForm"]["compte"];               
-    var email = document.forms["RegForm"]["somme"]; 
+console.log(compte)
+console.log(somme)
 
-    if (name.value == "")                                  
-    { 
-        alert("Mettez votre nom."); 
-        name.focus(); 
-        return false; 
-    }    
-    if (address.value == "")                               
-    { 
-        alert("Mettez votre adresse."); 
-        address.focus(); 
-        return false; 
-    }        
-    if (email.value == "")                                   
-    { 
-        alert("Mettez une adresse email valide."); 
-        email.focus(); 
-        return false; 
-    }    
-    if (email.value.indexOf("@", 0) < 0)                 
-    { 
-        alert("Mettez une adresse email valide."); 
-        email.focus(); 
-        return false; 
-    }    
-    if (email.value.indexOf(".", 0) < 0)                 
-    { 
-        alert("Mettez une adresse email valide."); 
-        email.focus(); 
-        return false; 
-    }    
-    if (phone.value == "")                           
-    { 
-        alert("Mettez votre numéro de téléphone."); 
-        phone.focus(); 
-        return false; 
-    }    
-    if (password.value == "")                        
-    { 
-        alert("Saisissez votre mot de passe"); 
-        password.focus(); 
-        return false; 
-    }    
-    if (what.selectedIndex < 1)                  
-    { 
-        alert("Mettez votre cours."); 
-        what.focus(); 
-        return false; 
-    } 
-    if (comment.value == "")                  
-    { 
-        alert("Écrivez un commentaire."); 
-        comment.focus(); 
-        return false; 
-    } 
-    return true; 
+//on vérifie les infos du compte en banque 
+
+if (compte.lenght == 16) {
+    validc = 'valide';
+    //si le numéro est valide on change sa couleur 
+    compte.style.backgroundColor = "#6eff33"; 
+  } else {
+    validc = 'non valide';
+    erreur_alert()
+  }
+
+//on vérifie que la somme est correct (>0)
+
+if (somme > 0) {
+    valids = 'valide';
+    somme_alert
+  } else {
+    valids = 'non valide';
+    erreur_alert()
+  }
+
+
+//création du fonction pour les alert
+//fonction alert (somme envoyé)
+
+function somme_alert() {
+    alert("xxxxxx");
+  }
+
+//fonction alert (erreur)
+
+function erreur_alert() {
+    alert("Erreur sur le champs")
 }
